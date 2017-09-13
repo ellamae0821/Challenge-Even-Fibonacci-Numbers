f96e54fb7b798f649a29e8c2729f06bae6e22cd2
Ella Mae A <ellamae207@gmail.com>
2017-09-13 09:32:33 -1000

    passes 3

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,11 +7,20 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+  var fibSequence = [0 , 1];
+
+
 
   // do your work here
-
+  while (maxFibValue > fibSequence[fibSequence.length-1]){
+    fibSequence.push(fibSequence[fibSequence.length-1] + fibSequence[fibSequence.length-2]);
+    if (fibSequence[fibSequence.length-1] % 2 === 0){
+      sum += fibSequence[fibSequence.length-1];
+    }
+  }
   return sum;
 }
+console.log(_sumFibs(4000000));
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
@@ -19,9 +28,17 @@ function _highestFibonacciNumber (maxFibValue){
 
   //define your base case, validate your input
 
+  var fibSequence = [0 , 1];
+
 
   //do your work here
-
+  while (maxFibValue > fibSequence[fibSequence.length-1]){
+    fibSequence.push(fibSequence[fibSequence.length-1] + fibSequence[fibSequence.length-2]);
+    if (fibSequence[fibSequence.length-1] % 2 === 0){
+      highest = fibSequence[fibSequence.length-2];
+      console.log(highest);
+    }
+  }
   return highest;
 };
 
